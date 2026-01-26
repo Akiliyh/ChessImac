89ffd04d7a4d689e4ee6889f51d8abb118b73047
Guillaume Boucher <guillaumeboucher.contact@gmail.com>
2026-01-26 16:07:19 +0100

    init gameboard ui

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <imgui.h>
 #include <iostream>
+#include <string>
 #include "quick_imgui/quick_imgui.hpp"
 
 int main()
@@ -16,27 +17,38 @@ int main()
 
                     ImGui::Begin("Example");
 
-                    ImGui::SliderFloat("My Value", &value, 0.f, 3.f);
+                    const int BOARD_SIZE{8}; // 8x8
 
-                    if (ImGui::Button("1", ImVec2{50.f, 50.f}))
-                        std::cout << "Clicked button 1\n";
-                    ImGui::SameLine(); // Draw the next ImGui widget on the same line as the previous one. Otherwise it would be below it
+                    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
+                    {
+                        int row = i / BOARD_SIZE;
+                        int col = i % BOARD_SIZE;
 
-                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.f, 0.f, 1.f}); // Changes the color of all buttons until we call ImGui::PopStyleColor(). There is also ImGuiCol_ButtonActive and ImGuiCol_ButtonHovered
+                        bool isDark = (row + col) % 2 == 1;
+                        if (isDark)
+                        {
+                            ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.f, 0.f, 1.f});
+                        }
+                        else
+                        {
+                            ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.5f, 0.f, 1.f});
+                        }
 
-                    ImGui::PushID(2); // When some ImGui items have the same label (for exemple the next two buttons are labeled "Yo") ImGui needs you to specify an ID so that it can distinguish them. It can be an int, a pointer, a string, etc.
-                                      // You will definitely run into this when you create a button for each of your chess pieces, so remember to give them an ID!
-                    if (ImGui::Button("Yo", ImVec2{50.f, 50.f}))
-                        std::cout << "Clicked button 2\n";
-                    ImGui::PopID(); // Then pop the id you pushed after you created the widget
+                        ImGui::PushID(i);
+                        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0.0f, 0.0f));
+                        if (ImGui::Button(std::to_string(i).c_str(), ImVec2{50.f, 50.f}))
+                        {
+                            std::cout << "Clicked button " << i << "\n";
+                        }
+                        ImGui::PopStyleVar();
+                        ImGui::PopID();
+                        ImGui::PopStyleColor();
 
-                    ImGui::SameLine();
-                    ImGui::PushID(3);
-                    if (ImGui::Button("Yo", ImVec2{50.f, 50.f}))
-                        std::cout << "Clicked button 3\n";
-                    ImGui::PopID();
-
-                    ImGui::PopStyleColor();
+                        if ((i % BOARD_SIZE) != (BOARD_SIZE - 1))
+                        {
+                            ImGui::SameLine(0, 0); // we smash the tiles together
+                        }
+                    }
 
                     ImGui::End();
                 },
