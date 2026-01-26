d8701031f2306068971a5447bab48f3e934a5ad6
Lilian Peuron <lilianpeuron@gmail.com>
2026-01-26 21:18:28 +0100

    Initialize Bishop

--- src/Pieces.cpp
+++ src/Pieces.cpp
@@ -5,6 +5,7 @@ Rook::Rook(const int position, const bool white): Piece(position, white) {};
 
 std::vector<int> Rook::get_moves(std::vector<Piece*>& board){
     std::vector<int> free_case;
+    int i {};
     // std::vector<int> take_case;
 
     // std::vector<std::vector<int>> moves;
@@ -41,22 +42,7 @@ std::vector<int> Rook::get_moves(std::vector<Piece*>& board){
         }
     }
 
-    // for (int i = m_position + 1 ; i < 64 ; i += 1) {
-    //     int next_case {i};
-    //     if (board[next_case] == nullptr) {
-    //         // board[m_position + 1] => color space free;
-    //         free_case.push_back(next_case);
-    //     } 
-    //     else 
-    //     {
-    //         if (this->m_white != board[next_case]->m_white) {                    
-    //             // board[m_position + 1] => color space taken;
-    //         free_case.push_back(next_case);
-    //         }
-    //     }
-    // }
-
-    int i = m_position + 1;
+    i = m_position + 1;
     while (i < 64 && (i / 8) == (m_position / 8)) {
         if (board[i] == nullptr) {
             free_case.push_back(i);
@@ -69,22 +55,6 @@ std::vector<int> Rook::get_moves(std::vector<Piece*>& board){
         }
         i++;
     }
-
-
-    // for (int i = m_position - 1 ; i >= 0 ; i -= 1) {
-    // int next_case {i};
-    //     if (board[next_case] == nullptr) {
-    //         // board[m_position - 1] => color space free;
-    //         free_case.push_back(next_case);
-    //     } 
-    //     else 
-    //     {
-    //         if (this->m_white != board[next_case]->m_white) {                    
-    //             // board[m_position - 1] => color space taken;
-    //         free_case.push_back(next_case);
-    //         }
-    //     }
-    // }
 
     i = m_position - 1;
     while (i >= 0 && (i / 8) == (m_position / 8)) {
@@ -107,10 +77,74 @@ std::vector<int> Rook::get_moves(std::vector<Piece*>& board){
     return free_case;
 }
 
-// std::vector<std::vector<int>> Bishop::get_moves(std::vector<Piece*>& board){
-    
-// };
+Bishop::Bishop(const int position, const bool white): Piece(position, white) {};
 
-// std::vector<int> Bishop::get_moves(const Board& board){
-    
-// };
+std::vector<int> Bishop::get_moves(std::vector<Piece*>& board){
+    std::vector<int> free_case;
+    int i {};
+    int prev {};
+
+    i = m_position + 9;
+    prev = m_position;
+
+    while (i < 64 && abs((i % 8) - (prev % 8)) == 1) {
+        if (!board[i]) {
+            free_case.push_back(i);
+        } else {
+            if (board[i]->m_white != m_white)
+                free_case.push_back(i);
+            break;
+        }
+        prev = i;
+        i += 9;
+    }
+
+    i = m_position - 9;
+    prev = m_position;
+
+    while (i >= 0 && abs((i % 8) - (prev % 8)) == 1) {
+        if (!board[i]) {
+            free_case.push_back(i);
+        } else {
+            if (board[i]->m_white != m_white)
+                free_case.push_back(i);
+            break;
+        }
+        prev = i;
+        i -= 9;
+    }
+
+
+    i = m_position + 7;
+    prev = m_position;
+
+    while (i < 64 && abs((i % 8) - (prev % 8)) == 1) {
+        if (!board[i]) {
+            free_case.push_back(i);
+        } else {
+            if (board[i]->m_white != m_white)
+                free_case.push_back(i);
+            break;
+        }
+        prev = i;
+        i += 7;
+    }
+
+    i = m_position - 7;
+    prev = m_position;
+
+    while (i >= 0 && abs((i % 8) - (prev % 8)) == 1) {
+        if (!board[i]) {
+            free_case.push_back(i);
+        } else {
+            if (board[i]->m_white != m_white)
+                free_case.push_back(i);
+            break;
+        }
+        prev = i;
+        i -= 7;
+    }
+
+
+    return free_case;
+};
--- src/Pieces.hpp
+++ src/Pieces.hpp
@@ -19,5 +19,6 @@ class Rook : public Piece {
 
 class Bishop : public Piece {
     public:
-        std::vector<std::vector<int>> get_moves(std::vector<Piece*>& board);
+        Bishop(const int position, const bool white);
+        std::vector<int> get_moves(std::vector<Piece*>& board);
 };
