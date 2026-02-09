59f9b28d08bc994117f07c7d3373cb3ca6491746
Guillaume Boucher <guillaumeboucher.contact@gmail.com>
2026-02-09 16:49:37 +0100

    refactored renderer

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -9,10 +9,47 @@
 #include "Pieces.hpp"
 #include "quick_imgui/quick_imgui.hpp"
 
+void Renderer::display_possible_moves(Chessboard& board, Piece* current_square, Piece* previous_square, std::vector<int> &possible_moves)
+{
+    if (current_square != nullptr)
+    {
+        if (current_square->on_focus)
+        {
+            current_square->on_focus = false;
+            possible_moves.clear();
+        }
+        else
+        {
+            possible_moves.clear();
+            possible_moves = current_square->get_moves(board.board_data);
+            for (int possible_move : possible_moves)
+            {
+                std::cout << possible_move << '\n';
+            }
+            current_square->on_focus = true;
+
+            if (previous_square != nullptr)
+            {
+                previous_square->on_focus = false;
+            }
+        }
+
+        previous_square = current_square;
+    }
+    else
+    {
+        if (previous_square != nullptr)
+        {
+            previous_square->on_focus = false;
+        }
+        possible_moves.clear();
+    }
+}
+
 void Renderer::draw(Chessboard& board)
 {
     std::vector<int> possible_moves{};
-    Piece*      previous_square{};
+    Piece*           previous_square{};
 
     quick_imgui::loop(
         "ChessImac",
@@ -21,97 +58,71 @@ void Renderer::draw(Chessboard& board)
             .loop =
 
                 [&]() {
-                    ImGui::ShowDemoWindow(); // This opens a window which shows tons of examples of what you can do with ImGui. You should check it out! Also, you can use the "Item Picker" in the top menu of that demo window: then click on any widget and it will show you the corresponding code directly in your IDE!
+            ImGui::ShowDemoWindow(); // This opens a window which shows tons of examples of what you can do with ImGui. You should check it out! Also, you can use the "Item Picker" in the top menu of that demo window: then click on any widget and it will show you the corresponding code directly in your IDE!
 
-                    ImGui::Begin("ChessImac");
+            ImGui::Begin("ChessImac");
 
-                    const int BOARD_SIZE{board.get_size()}; // 8x8
+            const int BOARD_SIZE{board.get_size()}; // 8x8
 
-                    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
+            for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
+            {
+                int row = i / BOARD_SIZE;
+                int col = i % BOARD_SIZE;
+
+                auto it = std::find(possible_moves.begin(), possible_moves.end(), i);
+
+                // Check if we should show the border
+                bool should_border = (it != possible_moves.end());
+
+                if (should_border)
+                {
+                    ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 5.0f);
+                }
+
+                bool isDark = (row + col) % 2 == 1;
+                if (isDark)
+                {
+                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.f, 0.f, 1.f});
+                }
+                else
+                {
+                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.5f, 0.f, 1.f});
+                }
+
+                ImGui::PushID(i);
+                ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0.0f, 0.0f));
+
+                std::string label{};
+                Piece*      current_square{board.board_data[i]};
+
+                if (current_square != nullptr)
+                    label = current_square->m_label;
+
+                if (ImGui::Button(label.c_str(), ImVec2{50.f, 50.f}))
+                {
+                    std::cout << "Clicked button " << i << "\n";
+
+                    Renderer::display_possible_moves(board, current_square, previous_square, possible_moves);
+}
+                    if (should_border)
                     {
-                        int row = i / BOARD_SIZE;
-                        int col = i % BOARD_SIZE;
-
-                        auto it = std::find(possible_moves.begin(), possible_moves.end(), i);
-
-                        // Check if we should show the border
-                        bool should_border = (it != possible_moves.end());
-
-                        if (should_border)
-                        {
-                            ImGui::PushStyleVar(ImGuiStyleVar_FrameBorderSize, 2.0f);
-                        }
-
-                        bool isDark = (row + col) % 2 == 1;
-                        if (isDark)
-                        {
-                            ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.f, 0.f, 1.f});
-                        }
-                        else
-                        {
-                            ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{1.f, 0.5f, 0.f, 1.f});
-                        }
-
-                        ImGui::PushID(i);
-                        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0.0f, 0.0f));
-
-                        std::string label{};
-                        Piece*      current_square{board.board_data[i]};
-
-                        if (current_square != nullptr)
-                            label = current_square->m_label;
-
-                        if (ImGui::Button(label.c_str(), ImVec2{50.f, 50.f}))
-                        {
-                            std::cout << "Clicked button " << i << "\n";
-
-                            if (current_square != nullptr)
-                            {
-                                if (current_square->on_focus)
-                                {
-                                    current_square->on_focus = false;
-                                    possible_moves.clear();
-                                    
-                                } else {
-                                    possible_moves.clear();
-                                    possible_moves = current_square->get_moves(board.board_data);
-                                    for (int possible_move : possible_moves) {
-                                        std::cout << possible_move << std::endl;
-                                    }
-                                    current_square->on_focus = true;
-                                    
-                                    if (previous_square != nullptr) {
-                                        previous_square->on_focus = false;
-                                    }
-                                }
-
-                                previous_square = current_square;
-
-                            } else {
-                                if (previous_square != nullptr) {
-                                    previous_square->on_focus = false;
-                                }
-                                possible_moves.clear();
-                            }
-                        }
-
-                        if (should_border)
-                        {
-                            ImGui::PopStyleVar();
-                        }
-
                         ImGui::PopStyleVar();
-                        ImGui::PopID();
-                        ImGui::PopStyleColor();
-
-                        if ((i % BOARD_SIZE) != (BOARD_SIZE - 1))
-                        {
-                            ImGui::SameLine(0, 0); // we smash the tiles together
-                        }
                     }
 
-                    ImGui::End();
-                },
+                    ImGui::PopStyleVar();
+                    ImGui::PopID();
+                    ImGui::PopStyleColor();
+
+                    if ((i % BOARD_SIZE) != (BOARD_SIZE - 1))
+                    {
+                        ImGui::SameLine(0, 0); // we smash the tiles together
+                    }
+                
+            }
+
+                ImGui::End();
+            }
+            ,
         }
     );
 }
--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -7,5 +7,6 @@ class Renderer {
 
     public:
         void draw(Chessboard& board);
+        void display_possible_moves(Chessboard& board, Piece* current_square, Piece* previous_square, std::vector<int> &possible_moves);
         Renderer() = default;
 };
