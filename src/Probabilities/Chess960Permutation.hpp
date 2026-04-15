f03b442b151cc84a41ae0994df270c4dd198bccc
Lilian Peuron <lilianpeuron@gmail.com>
2026-04-15 15:21:38 +0200

    Upd comments

--- src/Probabilities/Chess960Permutation.hpp
+++ src/Probabilities/Chess960Permutation.hpp
@@ -5,30 +5,29 @@
 
 class Chess960Permutation {
   private:
-    std::mt19937            rng;
-    const std::vector<char> basePieces = {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
+    std::mt19937            m_random;
+    const std::vector<char> m_base_pieces = {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
 
-    bool        isValidChess960(const std::vector<char>& rank) const;
-    std::string buildFen(const std::vector<char>& backRank) const;
-    void        customFisherYatesShuffle(std::vector<char>& vec);
+    bool        is_valid_chess960(const std::vector<char>& rank) const;
+    std::string build_fen(const std::vector<char>& back_rank) const;
+    void        custom_fisher_yates_shuffle(std::vector<char>& vec);
 
   public:
     Chess960Permutation();
-    std::string generateWithLibrary();
-    std::string generateFromScratch();
+    std::string generate_std();
+    std::string generate_scratch();
 
-    // Nouvelle version de la comparaison
-    void compareMethods(int iterations);
+    void compare_methods(int iterations);
 };
 
 // Chess960Permutation generator;
 
 // std::cout << "Generation via Standard Library :\n";
-// std::cout << generator.generateWithLibrary() << "\n\n";
+// std::cout << generator.generate_std() << "\n\n";
 
 // std::cout << "Generation via From Scratch :\n";
-// std::cout << generator.generateFromScratch() << "\n\n";
+// std::cout << generator.generate_scratch() << "\n\n";
 
 // Lance une comparaison statistique lourde (ex: 1 million de parties)
 // Cela prendra quelques secondes mais te prouvera la qualite de ta permutation.
-// generator.compareMethods(1000000);
+// generator.compare_methods(1000000);
