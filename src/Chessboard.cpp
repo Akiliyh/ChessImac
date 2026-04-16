447823f34085545a4d0c09ff49ef44b3d7f01813
Lilian Peuron <lilianpeuron@gmail.com>
2026-04-16 12:09:12 +0200

    Block Move_piece during pause

--- src/Chessboard.cpp
+++ src/Chessboard.cpp
@@ -95,121 +95,127 @@ bool Chessboard::move_piece(
     std::unique_ptr<Piece>& active_square, int dest_position, GameManager& game
 )
 {
-    std::vector<int> legal_moves = active_square->get_moves(this->board_data);
-
-    // Is the move legal ?
-    if (std::find(legal_moves.begin(), legal_moves.end(), dest_position) != legal_moves.end())
+    if (!game.is_paused())
     {
-        game.reset_turn_timer();
+        std::vector<int> legal_moves = active_square->get_moves(this->board_data);
 
-        if (game.get_mode() == GameMode::Classic)
+        // Is the move legal ?
+        if (std::find(legal_moves.begin(), legal_moves.end(), dest_position) != legal_moves.end())
         {
-            active_square->update_position(dest_position);
-            if (active_square->is_first_move())
-                active_square->update_first_move(false);
-            if (this->board_data[dest_position] != nullptr)
-            {
-                dead_pieces.push_back(std::move(this->board_data[dest_position]));
-            }
-            this->board_data[dest_position] = std::move(active_square);
-            return true;
-        }
+            game.reset_turn_timer();
 
-        if (game.get_mode() == GameMode::Chaos)
-        {
-            // Is there an enemy ?
-            if (this->board_data[dest_position] != nullptr)
+            if (game.get_mode() == GameMode::Classic)
             {
-                double    p = 0.75; // 75% to hit the enemy
-                Bernoulli random_dodge(p);
-
-                if (!random_dodge.generate_scratch())
+                active_square->update_position(dest_position);
+                if (active_square->is_first_move())
+                    active_square->update_first_move(false);
+                if (this->board_data[dest_position] != nullptr)
                 {
-                    game.trigger_dodge_popup("Bad Luck ! Your enemy dodged the attack.");
-                    return true;
+                    dead_pieces.push_back(std::move(this->board_data[dest_position]));
+                }
+                this->board_data[dest_position] = std::move(active_square);
+                return true;
+            }
+
+            if (game.get_mode() == GameMode::Chaos)
+            {
+                // Is there an enemy ?
+                if (this->board_data[dest_position] != nullptr)
+                {
+                    double    p = 0.75; // 75% to hit the enemy
+                    Bernoulli random_dodge(p);
+
+                    if (!random_dodge.generate_scratch())
+                    {
+                        game.trigger_dodge_popup("Bad Luck ! Your enemy dodged the attack.");
+                        return true;
+                    }
+
+                    // Attack success
+                    dead_pieces.push_back(std::move(this->board_data[dest_position]));
                 }
 
-                // Attack success
-                dead_pieces.push_back(std::move(this->board_data[dest_position]));
-            }
+                active_square->update_position(dest_position);
 
-            active_square->update_position(dest_position);
-
-            if (active_square->is_first_move())
-            {
-                active_square->update_first_move(false);
-            }
-
-            active_square->up_move_count();
-
-            // Is the Piece mutating ?
-            WeibullEvolution chessEvolution(2.0, 3.0);
-            Bernoulli        random_evolution(
-                chessEvolution.get_probability_scratch(active_square->get_move_count())
-            );
-            bool will_evolve = random_evolution.generate_scratch();
-
-            this->board_data[dest_position] = std::move(active_square);
-
-            bool is_king = dynamic_cast<King*>(this->board_data[dest_position].get()) != nullptr;
-
-            if (will_evolve && !is_king)
-            {
-                PieceColor current_color = this->board_data[dest_position]->get_color();
-
-                int dest_x = dest_position % 8;
-                int dest_y = dest_position / 8;
-
-                int random_type = std::rand() % 5; // Un nombre entre 0 et 3
-
-                std::string piece_nom                   = "";
-                std::string mutation_expression_feeling = "";
-
-                switch (random_type)
+                if (active_square->is_first_move())
                 {
-                case 0:
-                    this->board_data[dest_position] =
-                        std::make_unique<Queen>(dest_x, dest_y, current_color);
-                    piece_nom                   = "Queen";
-                    mutation_expression_feeling = "Outstanding !";
-                    break;
-                case 1:
-                    this->board_data[dest_position] =
-                        std::make_unique<Rook>(dest_x, dest_y, current_color);
-                    piece_nom                   = "Rook";
-                    mutation_expression_feeling = "Amazing !";
-                    break;
-                case 2:
-                    this->board_data[dest_position] =
-                        std::make_unique<Bishop>(dest_x, dest_y, current_color);
-                    piece_nom                   = "Bishop";
-                    mutation_expression_feeling = "Awesome !";
-                    break;
-                case 3:
-                    this->board_data[dest_position] =
-                        std::make_unique<Knight>(dest_x, dest_y, current_color);
-                    piece_nom                   = "Knight";
-                    mutation_expression_feeling = "Great !";
-                    break;
-                case 4:
-                    this->board_data[dest_position] =
-                        std::make_unique<Pawn>(dest_x, dest_y, current_color);
-                    piece_nom                   = "Pawn";
-                    mutation_expression_feeling = "Oops...";
-                    break;
+                    active_square->update_first_move(false);
                 }
 
-                game.trigger_mutation_popup(
-                    mutation_expression_feeling + " Your piece becomes a " + piece_nom + " !"
+                active_square->up_move_count();
+
+                // Is the Piece mutating ?
+                WeibullEvolution chessEvolution(2.0, 3.0);
+                Bernoulli        random_evolution(
+                    chessEvolution.get_probability_scratch(active_square->get_move_count())
                 );
-            }
+                bool will_evolve = random_evolution.generate_scratch();
 
-            return true;
+                this->board_data[dest_position] = std::move(active_square);
+
+                bool is_king =
+                    dynamic_cast<King*>(this->board_data[dest_position].get()) != nullptr;
+
+                if (will_evolve && !is_king)
+                {
+                    PieceColor current_color = this->board_data[dest_position]->get_color();
+
+                    int dest_x = dest_position % 8;
+                    int dest_y = dest_position / 8;
+
+                    int random_type = std::rand() % 5; // Un nombre entre 0 et 3
+
+                    std::string piece_nom                   = "";
+                    std::string mutation_expression_feeling = "";
+
+                    switch (random_type)
+                    {
+                    case 0:
+                        this->board_data[dest_position] =
+                            std::make_unique<Queen>(dest_x, dest_y, current_color);
+                        piece_nom                   = "Queen";
+                        mutation_expression_feeling = "Outstanding !";
+                        break;
+                    case 1:
+                        this->board_data[dest_position] =
+                            std::make_unique<Rook>(dest_x, dest_y, current_color);
+                        piece_nom                   = "Rook";
+                        mutation_expression_feeling = "Amazing !";
+                        break;
+                    case 2:
+                        this->board_data[dest_position] =
+                            std::make_unique<Bishop>(dest_x, dest_y, current_color);
+                        piece_nom                   = "Bishop";
+                        mutation_expression_feeling = "Awesome !";
+                        break;
+                    case 3:
+                        this->board_data[dest_position] =
+                            std::make_unique<Knight>(dest_x, dest_y, current_color);
+                        piece_nom                   = "Knight";
+                        mutation_expression_feeling = "Great !";
+                        break;
+                    case 4:
+                        this->board_data[dest_position] =
+                            std::make_unique<Pawn>(dest_x, dest_y, current_color);
+                        piece_nom                   = "Pawn";
+                        mutation_expression_feeling = "Oops...";
+                        break;
+                    }
+
+                    game.trigger_mutation_popup(
+                        mutation_expression_feeling + " Your piece becomes a " + piece_nom + " !"
+                    );
+                }
+
+                return true;
+            }
         }
-    }
-    else
-    {
-        std::cout << "Illegal move!" << '\n';
+        else
+        {
+            std::cout << "Illegal move!" << '\n';
+            return false;
+        }
+
         return false;
     }
 
